a12cc1907380cf68141610939c08b11dca007ce0
muliqitai <15026712203@163.com>
2021-10-17 17:12:04 +0800

    add the judge(mask is true/false), error: select one row data

--- python/interface_3.py
+++ python/interface_3.py
@@ -29,9 +29,11 @@ test_df = pd.read_csv(path_validation, header=0)
 
 train_df = pd.read_csv(path_train, header=0)  # pass header=0 to be able to replace existing names
 
+test=test_df.sample(n=1)
+train=train_df.sample(n=1)
 
-train_AP_features = scale(np.asarray(train_df.iloc[:, 0:520]).astype(float),axis=1)  # convert integer to float and scale jointly (axis=1)
-train_df['REFPOINT'] = train_df.apply(lambda row: str(int(row['SPACEID'])) + str(int(row['RELATIVEPOSITION'])), axis=1)# add a new column
+train_AP_features = scale(np.asarray(train.iloc[:,0:520]).astype(float),axis=1)  # convert integer to float and scale jointly (axis=1)
+train_df['REFPOINT'] = train.apply(lambda row: str(int(row['SPACEID'])) + str(int(row['RELATIVEPOSITION'])), axis=1)# add a new column
 blds = np.unique(train_df[['BUILDINGID']])
 flrs = np.unique(train_df[['FLOOR']])
 x_avg = {}
@@ -46,10 +48,12 @@ for bld in blds:
         # calculate the average coordinates of each building/floor
         x_avg[str(bld) + '-' + str(flr)] = np.mean(train_df.loc[cond, 'LONGITUDE'])
         y_avg[str(bld) + '-' + str(flr)] = np.mean(train_df.loc[cond, 'LATITUDE'])
-test_AP_features = scale(np.asarray(test_df.iloc[:, 0:520]).astype(float),axis=1)  # convert integer to float and scale jointly (axis=1)
+
+test_AP_features = scale(np.asarray(test.iloc[:,0:520]).astype(float),axis=1)  # convert integer to float and scale jointly (axis=1)
 blds_all = np.asarray(pd.get_dummies(pd.concat([train_df['BUILDINGID'], test_df['BUILDINGID']]))) # for consistency in one-hot encoding for both dataframes
 flrs_all = np.asarray(pd.get_dummies(pd.concat([train_df['FLOOR'], test_df['FLOOR']])))
 len_train = len(train_df)
+
 blds = blds_all[:len_train]
 flrs = flrs_all[:len_train]
 rfps = np.asarray(pd.get_dummies(train_df['REFPOINT']))
@@ -57,8 +61,8 @@ train_labels = np.concatenate((blds, flrs, rfps), axis=1)
 
 # turn the given validation set into a testing set
  # convert integer to float and scale jointly (axis=1)
-x_test_utm = np.asarray(test_df['LONGITUDE'])
-y_test_utm = np.asarray(test_df['LATITUDE'])
+x_test_utm = np.asarray(test['LONGITUDE'])
+y_test_utm = np.asarray(test['LATITUDE'])
 blds = blds_all[len_train:]
 flrs = flrs_all[len_train:]
 
@@ -67,13 +71,16 @@ print("\nPart 3: evaluating the model ...")
 
 # calculate the building and floor estimation
 preds = model.predict(test_AP_features, batch_size=batch_size)  #information about the buidling...
-blds_results = (np.equal(np.argmax(blds, axis=1), np.argmax(preds[:, :3], axis=1))).astype(int)
-flrs_results = (np.equal(np.argmax(flrs, axis=1), np.argmax(preds[:, 3:8], axis=1))).astype(int)
+blds_results = (np.equal(np.argmax(blds, axis=1), np.argmax(preds[0:1, :3], axis=1))).astype(int)
+flrs_results = (np.equal(np.argmax(flrs, axis=1), np.argmax(preds[0:1, 3:8], axis=1))).astype(int)
 mask = np.logical_and(blds_results, flrs_results)
-rfps = (preds[mask])[:, 8:118]
+rfps = (preds[mask])[0:1, 8:118]
 print('preds',preds)
 print('building',blds_results)
 print('floor',flrs_results)
+print('mask',mask)
+if mask == [False]:
+    print('The prediction result is False')
 #print('rfps',preds[0:1, 8:118])
 #print('rfps',rfps)
 
@@ -90,7 +97,7 @@ x_test_utm = x_test_utm[mask]
 y_test_utm = y_test_utm[mask]
 blds = blds[mask]
 flrs = flrs[mask]
-rfps = (preds[mask])[:, 8:118]
+rfps = (preds[mask])[0:1, 8:118]
 n_success = len(blds)  # number of correct building and floor location
 n_loc_failure = 0
 sum_pos_err = 0.0
@@ -108,8 +115,8 @@ for i in range(n_success):
         rows = np.where((train_labels == np.concatenate((blds[i], flrs[i], rfp))).all(axis=1)) # tuple of row indexes
         if rows[0].size > 0:
             if rfps[i][j] >= threshold[i]:
-                xs.append(train_df.loc[train_df.index[rows[0][0]], 'LONGITUDE'])
-                ys.append(train_df.loc[train_df.index[rows[0][0]], 'LATITUDE'])
+                xs.append(train.loc[train.index[rows[0][0]], 'LONGITUDE'])
+                ys.append(train.loc[train.index[rows[0][0]], 'LATITUDE'])
                 ws.append(rfps[i][j])
     if len(xs) > 0:
         sum_pos_err += math.sqrt((np.mean(xs) - x_test_utm[i]) ** 2 + (np.mean(ys) - y_test_utm[i]) ** 2)
@@ -120,7 +127,8 @@ for i in range(n_success):
     else:
         n_loc_failure += 1
         key = str(np.argmax(blds[i])) + '-' + str(np.argmax(flrs[i]))
-print('Cor',Cor)
+if mask == [True]:
+    print('Cor',Cor)
 
 
 

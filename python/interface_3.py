299c0dafc363f135f00a44dc2541cd638e15b4d2
muliqitai <15026712203@163.com>
2021-10-29 17:56:59 +0800

    select one row of dataset

--- python/interface_3.py
+++ python/interface_3.py
@@ -32,8 +32,8 @@ train_df = pd.read_csv(path_train, header=0)  # pass header=0 to be able to repl
 test=test_df.sample(n=1)
 train=train_df.sample(n=1)
 
-train_AP_features = scale(np.asarray(train.iloc[:,0:520]).astype(float),axis=1)  # convert integer to float and scale jointly (axis=1)
-train_df['REFPOINT'] = train.apply(lambda row: str(int(row['SPACEID'])) + str(int(row['RELATIVEPOSITION'])), axis=1)# add a new column
+train_AP_features = scale(np.asarray(train_df.iloc[:,0:520]).astype(float),axis=1)  # convert integer to float and scale jointly (axis=1)
+train_df['REFPOINT'] = train_df.apply(lambda row: str(int(row['SPACEID'])) + str(int(row['RELATIVEPOSITION'])), axis=1)# add a new column
 blds = np.unique(train_df[['BUILDINGID']])
 flrs = np.unique(train_df[['FLOOR']])
 x_avg = {}
@@ -61,6 +61,9 @@ train_labels = np.concatenate((blds, flrs, rfps), axis=1)
 
 # turn the given validation set into a testing set
  # convert integer to float and scale jointly (axis=1)
+blds_all = np.asarray(pd.get_dummies(pd.concat([train['BUILDINGID'], test['BUILDINGID']]))) # for consistency in one-hot encoding for both dataframes
+flrs_all = np.asarray(pd.get_dummies(pd.concat([train['FLOOR'], test['FLOOR']])))
+len_train = len(train)
 x_test_utm = np.asarray(test['LONGITUDE'])
 y_test_utm = np.asarray(test['LATITUDE'])
 blds = blds_all[len_train:]
@@ -71,10 +74,10 @@ print("\nPart 3: evaluating the model ...")
 
 # calculate the building and floor estimation
 preds = model.predict(test_AP_features, batch_size=batch_size)  #information about the buidling...
-blds_results = (np.equal(np.argmax(blds, axis=1), np.argmax(preds[0:1, :3], axis=1))).astype(int)
-flrs_results = (np.equal(np.argmax(flrs, axis=1), np.argmax(preds[0:1, 3:8], axis=1))).astype(int)
+blds_results = (np.equal(np.argmax(blds, axis=1), np.argmax(preds[:, :3], axis=1))).astype(int)
+flrs_results = (np.equal(np.argmax(flrs, axis=1), np.argmax(preds[:, 3:8], axis=1))).astype(int)
 mask = np.logical_and(blds_results, flrs_results)
-rfps = (preds[mask])[0:1, 8:118]
+rfps = (preds[mask])[:, 8:118]
 print('preds',preds)
 print('building',blds_results)
 print('floor',flrs_results)
